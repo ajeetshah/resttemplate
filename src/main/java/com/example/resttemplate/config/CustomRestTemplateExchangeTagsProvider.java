2001ab1ff00d8f825f91350bc19a724d719586af
Ajeet <ajeets.shah@hotmail.com>
2024-11-21 14:02:44 +0530

    fix tabs provider with spring boot 3

--- src/main/java/com/example/resttemplate/config/CustomRestTemplateExchangeTagsProvider.java
+++ src/main/java/com/example/resttemplate/config/CustomRestTemplateExchangeTagsProvider.java
@@ -1,35 +1,56 @@
 package com.example.resttemplate.config;
 
-import io.micrometer.core.instrument.Tag;
-import io.micrometer.core.instrument.Tags;
-import org.springframework.boot.actuate.metrics.web.client.RestTemplateExchangeTagsProvider;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpResponse;
+import io.micrometer.common.KeyValue;
+import io.micrometer.common.KeyValues;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.observation.ClientHttpObservationDocumentation;
+import org.springframework.http.client.observation.ClientRequestObservationContext;
+import org.springframework.http.client.observation.ClientRequestObservationConvention;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.IOException;
 import java.util.Objects;
 
+
 @Component
-public class CustomRestTemplateExchangeTagsProvider implements RestTemplateExchangeTagsProvider {
+public class CustomRestTemplateExchangeTagsProvider implements ClientRequestObservationConvention {
 
     @Override
-    public Iterable<Tag> getTags(String urlTemplate, HttpRequest request, ClientHttpResponse response) {
-        // Create custom tags based on the HTTP request and response
-        Tags tags = Tags.empty()
-                .and("method 1", Objects.requireNonNull(request.getMethod()).name())
-                .and("uri 2", request.getURI().toString());
+    public String getName() {
+        // We can customize the metric name as per our own requirement
+        return "http.client.requests";
+    }
 
-        if (response != null) {
-            // Add HTTP status code tag if response is available
-            try {
-                tags = tags.and("status 3", String.valueOf(response.getStatusCode().value()));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+    @Override
+    @NonNull
+    public KeyValues getLowCardinalityKeyValues(@NonNull ClientRequestObservationContext context) {
+        return KeyValues.of(method(context), status(context), exception(context));
+    }
+
+    protected KeyValue method(ClientRequestObservationContext context) {
+        // return KeyValue.of(ClientHttpObservationDocumentation.LowCardinalityKeyNames.METHOD, context.getCarrier().getMethod().toString());
+        return KeyValue.of("method 1", Objects.requireNonNull(context.getCarrier()).getMethod().toString());
+    }
+
+    protected KeyValue status(ClientRequestObservationContext context) {
+        String statusCode = "";
+        try {
+            statusCode = Integer.toString(context.getResponse().getStatusCode().value());
+        } catch (Exception e) {
+
         }
+        // return KeyValue.of(ClientHttpObservationDocumentation.LowCardinalityKeyNames.STATUS, statusCode);
+        return KeyValue.of("status 3", statusCode);
+    }
 
-        return tags;
+    protected KeyValue exception(ClientRequestObservationContext context) {
+        if (context.getError() != null) {
+            return KeyValue.of(ClientHttpObservationDocumentation.LowCardinalityKeyNames.EXCEPTION, context.getError().getClass().getName());
+        } else {
+            return KeyValue.of(ClientHttpObservationDocumentation.LowCardinalityKeyNames.EXCEPTION, "none");
+        }
     }
 
 }
